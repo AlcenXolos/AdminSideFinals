6bc597c343a6ddcf5d819bed0828513e23a41f10
Alcen Kapitbahay <kapitbahayalcen07@gmail.com>
2024-05-28 23:08:27 +0800

    API ROUTES for admin side add update delete

--- Laravel Folder/routes/api.php
+++ Laravel Folder/routes/api.php
@@ -11,10 +11,24 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/login', [LoginController::class, 'handleLogin']);
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/products', [productsController::class, 'productlist']);
+
 Route::prefix('cart')->group(function () {
     Route::post('/add', [CartController::class, 'addToCart']);
     // Route::post('/adminAddItem', [CartController::class, 'adminAddItem']);
     Route::get('/', [CartController::class, 'viewCart']);
     Route::put('/{productId}', [CartController::class, 'updateCartItem']);
     Route::delete('/{productId}', [CartController::class, 'removeFromCart']);
+});
+
+Route::prefix('products')->group(function () {
+
+    Route::post('/adminAddItem', [productsController::class, 'addAdminItem']);
+    Route::get('/', [ProductsController::class, 'productList']);
+    Route::get('/{id}', [ProductsController::class, 'getProductDetails']);
+    Route::put('/adminUpdateItem/{productId}', [productsController::class, 'adminUpdateItem']);
+    Route::delete('/{productId}', [productsController::class, 'adminRemoveItem']);
 });
