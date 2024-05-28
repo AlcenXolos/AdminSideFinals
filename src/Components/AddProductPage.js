08b93f80c89dc2a64bfa185fe0b5b0d3a8ced61c
carl <carlaarongalang@gmail.com>
2024-05-28 23:40:40 +0800

    backend of add product for admin

--- src/Components/AddProductPage.js
+++ src/Components/AddProductPage.js
@@ -3,7 +3,38 @@ import { Link } from 'react-router-dom';
 import ButtonComponent from './ButtonComponent';
 
 const AddProduct = () => {
+    const [productName, setProductName] = useState('');
+    const [price, setPrice] = useState('');
+    const [productDescription, setProductDescription] = useState('');
+
+    const handleAddProduct = async () => {
     
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/products/adminAddItem', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    productName,
+                    price,
+                    productDescription,
+                }),
+            });
+    
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Product added successfully', data);
+                window.location.href = '/AdminHomePage';
+                // You can redirect the user or show a success message here
+            } else {
+                console.error('Failed to add product', response.statusText);
+                // Handle error scenario here
+            }
+        } catch (error) {
+            console.error('An error occurred', error);
+        }
+    };
     return (
         <div>
             <div>
